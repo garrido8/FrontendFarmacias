d539eb676a039e936f578db6b8e3ec886b667574
garrido8 <adriangarrido15.9@gmail.com>
2025-05-12 16:38:47 +0200

    Añadidas funciones para obtener la ruta en coche y en bici

--- src/app/services/rutas.service.ts
+++ src/app/services/rutas.service.ts
@@ -2,23 +2,44 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RouteResponse } from '../interfaces/ruta.interface';
-import { apiKey, orsUrl } from '../../environments/environments';
+import { apiKey, bikeRoute, carRoute, walkingRoute } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RutasService {
 
-  private orsUrl: string = orsUrl;
+  private walkingRoute: string = walkingRoute;
+  private carRoute: string = carRoute;
+  private bikeRoute: string = bikeRoute;
+
   private apiKey: string = apiKey;
   private http: HttpClient = inject(HttpClient);
 
-  getWalkingRoute(startLat: number, startLng: number, endLat: number, endLng: number): Observable<RouteResponse> {
+  public getWalkingRoute(startLat: number, startLng: number, endLat: number, endLng: number): Observable<RouteResponse> {
     const params = new HttpParams()
       .set('api_key', this.apiKey)
       .set('start', `${startLng},${startLat}`) // Longitud primero
       .set('end', `${endLng},${endLat}`);   // Longitud primero
 
-    return this.http.get<RouteResponse>(this.orsUrl, { params });
+    return this.http.get<RouteResponse>(this.walkingRoute, { params });
   }
+
+  public getCarRoute(startLat: number, startLng: number, endLat: number, endLng: number): Observable<RouteResponse> {
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+      .set('start', `${startLng},${startLat}`) // Longitud primero
+      .set('end', `${endLng},${endLat}`);   // Longitud primero
+
+    return this.http.get<RouteResponse>(this.carRoute, { params });
+  }
+
+  public getBikeRoute(startLat: number, startLng: number, endLat: number, endLng: number): Observable<RouteResponse> {
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('start', `${startLng},${startLat}`) // Longitud primero
+      .set('end', `${endLng},${endLat}`);   // Longitud primero
+    return this.http.get<RouteResponse>(this.bikeRoute, { params });
+  }
+
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,7 @@
 export const apiKey: string = '5b3ce3597851110001cf6248dec7b420c7d041ecb9b641ea4c51ca0e';
 
-export const orsUrl: string = 'https://api.openrouteservice.org/v2/directions/foot-walking';
+export const walkingRoute: string = 'https://api.openrouteservice.org/v2/directions/foot-walking';
+
+export const carRoute: string = 'https://api.openrouteservice.org/v2/directions/driving-car';
+
+export const bikeRoute: string = 'https://api.openrouteservice.org/v2/directions/cycling-regular';
